6ea792f73c89488222b3867dafd9fb6f4c5bc5ad
nFrosto <nicolas.ortizc@mail.udp.cl>
2024-08-26 04:11:12 -0400

    Fix memory, time mejorada legibilidad

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,42 +1,28 @@
-import ujson
-from collections import defaultdict
+import ujson as json
+from collections import defaultdict, Counter
 from datetime import datetime
 from typing import List, Tuple
 import heapq
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    date_tweet_counts = defaultdict(int)
-    date_user_counts = defaultdict(lambda: defaultdict(int))
-    top_dates = []
-
-    def update_top_dates(date):
-        if len(top_dates) < 10:
-            heapq.heappush(top_dates, (date_tweet_counts[date], date))
-        elif date_tweet_counts[date] > top_dates[0][0]:
-            heapq.heapreplace(top_dates, (date_tweet_counts[date], date))
-
-    def process_line(line):
-        tweet = ujson.loads(line)
-        date = datetime.fromisoformat(tweet['date'].replace('Z', '+00:00')).date()
-        username = tweet['user']['username']
-        
-        date_tweet_counts[date] += 1
-        date_user_counts[date][username] += 1
-        update_top_dates(date)
+    date_tweet_counts = Counter()  # Contador de tweets por fecha
+    date_user_counts = defaultdict(Counter)  # Contador de tweets por usuario por fecha
 
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
-            process_line(line)
+            try:
+                tweet = json.loads(line)
+                date = datetime.fromisoformat(tweet['date'].replace('Z', '+00:00')).date()
+                username = tweet['user']['username']
+                
+                date_tweet_counts[date] += 1
+                date_user_counts[date][username] += 1
+            except (KeyError, ValueError, json.JSONDecodeError) as e:
+                print(f"Error al procesar la línea: {str(e)}")
+    
+    # Obtener las 10 fechas con más tweets
+    top_10_dates = heapq.nlargest(10, date_tweet_counts.items(), key=lambda x: x[1])
 
-    result = []
-    for _, date in sorted(top_dates, reverse=True):
-        top_user = max(date_user_counts[date], key=date_user_counts[date].get)
-        result.append((date, top_user))
-        # Liberar memoria
-        del date_user_counts[date]
-
-    # Liberar memoria
-    del date_tweet_counts
-    del date_user_counts
-
+    result = [(date, max(date_user_counts[date], key=date_user_counts[date].get)) for _, date in top_10_dates]
+    
     return result
